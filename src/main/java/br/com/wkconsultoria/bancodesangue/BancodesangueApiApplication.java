86da26f45191c9b36940ed5ba09369b8f282cef1
Jonathan Rovieri <jrovieri@gmail.com>
2020-12-21 09:24:08 -0300

    spring init project

---
+++ src/main/java/br/com/wkconsultoria/bancodesangue/BancodesangueApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wkconsultoria.bancodesangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodesangueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodesangueApiApplication.class, args);
+	}
+
+}
